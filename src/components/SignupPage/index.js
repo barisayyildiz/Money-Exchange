3dad0033e8edbba44248ec839327fd890c4188af
barisayyildiz <barisayyildiz.1799@gmail.com>
2021-10-16 12:04:11 +0300

    Login ve Signup sayfaları oluşturuldu

---
+++ src/components/SignupPage/index.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+import './style.scss'
+
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Row, Col, InputGroup } from 'react-bootstrap';
+
+
+function SignUp() {
+  const [validated, setValidated] = useState(false);
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("")
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
+  return (
+		<div className="form-wrapper"> 
+			<h2>Signup Form</h2>
+			<Form noValidate validated={validated} onSubmit={handleSubmit}>
+				<Col>
+					<Form.Group as={Row} controlId="validationCustomUsername">
+						{/* <Form.Label>Username</Form.Label> */}
+						<InputGroup hasValidation>
+							<Form.Control
+								type="text"
+								placeholder="Username"
+								aria-describedby="inputGroupPrepend"
+								required
+								onChange={({target:{value}}) => setUsername(value)}
+							/>
+							<Form.Control.Feedback type="invalid">
+								Please choose a username.
+							</Form.Control.Feedback>
+						</InputGroup>
+					</Form.Group>
+
+					<Form.Group as={Row} controlId="validationPassword">
+						{/* <Form.Label>Password</Form.Label> */}
+						<InputGroup hasValidation>
+							<Form.Control
+								type="password"
+								placeholder="Password"
+								aria-describedby="inputGroupPrepend"
+								required
+								onChange={({target:{value}}) => setPassword(value)}
+							/>
+							<Form.Control.Feedback type="invalid">
+								Please choose a password.
+							</Form.Control.Feedback>
+						</InputGroup>
+					</Form.Group>
+				
+					<Button className="mb-3" type="submit">Submit form</Button>
+				</Col>
+
+			</Form>
+			<a href="#">Already have an account?</a>
+		</div>
+  );
+}
+
+export default SignUp
